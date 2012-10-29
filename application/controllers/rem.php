556f7c1852818f7a47e0dc3d10489e6cd99dccd6
Gagan Preet Singh <s.gagan.preet@gmail.com>
2012-10-29 20:43:30 +0530

    rem repaired

--- application/controllers/rem.php
+++ application/controllers/rem.php
@@ -11,7 +11,7 @@ class Rem_Controller extends Base_Controller
 	public function action_prog()
 	{
 		$id 	=	Input::json();
-		$prog					=	Prog::find($id);
+		$prog	=	Prog::find($id->id);
 		if($prog->delete())
 		{
 			return 1;
@@ -22,7 +22,7 @@ class Rem_Controller extends Base_Controller
 	public function action_job()
 	{
 		$id 	=	Input::json();
-		$job					=	Job::find($id);
+		$job	=	Job::find($id->id);
 		if($job->delete())
 		{
 			return 1;
@@ -33,7 +33,7 @@ class Rem_Controller extends Base_Controller
 	public function action_scholar()
 	{
 		$id 	=	Input::json();
-		$scholar				=	Scholar::find($id);
+		$scholar=	Scholar::find($id->id);
 		if($scholar->delete())
 		{
 			return 1;
@@ -44,7 +44,7 @@ class Rem_Controller extends Base_Controller
 	public function action_location()
 	{
 		$id 	=	Input::json();
-		$location				=	Location::find($id);
+		$location				=	Location::find($id->id);
 		if($location->delete())
 		{
 			return 1;
@@ -55,7 +55,7 @@ class Rem_Controller extends Base_Controller
 	public function action_subject()
 	{
 		$id 	=	Input::json();
-		$subject				=	Subject::find($id);
+		$subject				=	Subject::find($id->id);
 		if($subject->delete())
 		{
 			return 1;
@@ -66,7 +66,7 @@ class Rem_Controller extends Base_Controller
 	public function action_skill()
 	{
 		$id 	=	Input::json();
-		$skill					=	Skill::find($id);
+		$skill					=	Skill::find($id->id);
 		if($skill->delete())
 		{
 			return 1;
@@ -77,7 +77,7 @@ class Rem_Controller extends Base_Controller
 	public function action_position()
 	{
 		$id 	=	Input::json();
-		$position				=	Position::find($id);
+		$position				=	Position::find($id->id);
 		if($position->delete())
 		{
 			return 1;
@@ -88,7 +88,7 @@ class Rem_Controller extends Base_Controller
 	public function action_ps()
 	{
 		$id 	=	Input::json();
-		$ps		=	Ps::find($id);
+		$ps		=	Ps::find($id->id);
 		if($ps->delete())
 		{
 			return 1;
@@ -99,7 +99,7 @@ class Rem_Controller extends Base_Controller
 	public function action_jpos()
 	{
 		$id 	=	Input::json();
-		$jpos	=	Jpos::find($id);
+		$jpos	=	Jpos::find($id->id);
 		if($jpos->delete())
 		{
 			return 1;
@@ -110,7 +110,7 @@ class Rem_Controller extends Base_Controller
 	public function action_ppos()
 	{
 		$id 	=	Input::json();
-		$ppos=	Ppos::find($id);
+		$ppos	=	Ppos::find($id->id);
 		if($ppos->delete())
 		{
 			return 1;
@@ -121,7 +121,7 @@ class Rem_Controller extends Base_Controller
 	public function action_spos()
 	{
 		$id 	=	Input::json();
-		$spos=	Spos::find($id);
+		$spos=	Spos::find($id->id);
 		if($spos->delete())
 		{
 			return 1;
@@ -132,7 +132,7 @@ class Rem_Controller extends Base_Controller
 	public function action_jsub()
 	{
 		$id 	=	Input::json();
-		$jsub=	Jsub::find($id);
+		$jsub=	Jsub::find($id->id);
 		if($jsub->delete())
 		{
 			return 1;
@@ -143,7 +143,7 @@ class Rem_Controller extends Base_Controller
 	public function action_psub()
 	{
 		$id 	=	Input::json();
-		$psub	=	Psub::find($id);
+		$psub	=	Psub::find($id->id);
 		if($psub->delete())
 		{
 			return 1;
@@ -154,7 +154,7 @@ class Rem_Controller extends Base_Controller
 	public function action_ssub()
 	{
 		$id 	=	Input::json();
-		$ssub=	Ssub::find($id);
+		$ssub=	Ssub::find($id->id);
 		if($ssub->delete())
 		{
 			return 1;
@@ -165,7 +165,7 @@ class Rem_Controller extends Base_Controller
 	public function action_jbranch()
 	{
 		$id 	=	Input::json();
-		$jbranch=	Jbranch::find($id);
+		$jbranch=	Jbranch::find($id->id);
 		if($jbranch->delete())
 		{
 			return 1;
@@ -176,7 +176,7 @@ class Rem_Controller extends Base_Controller
 	public function action_pbranch()
 	{
 		$id 	=	Input::json();
-		$pbranch=	Pbranch::find($id);
+		$pbranch=	Pbranch::find($id->id);
 		if($pbranch->delete())
 		{
 			return 1;
@@ -187,7 +187,7 @@ class Rem_Controller extends Base_Controller
 	public function action_sbranch()
 	{
 		$id 	=	Input::json();
-		$sbranch=	Sbranch::find($id);
+		$sbranch=	Sbranch::find($id->id);
 		if($sbranch->delete())
 		{
 			return 1;
